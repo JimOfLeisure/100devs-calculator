e632438f1592cb3c735be0530b74c3f02090f1ab
Jim Nelson <jim@jimnelson.us>
2022-04-07 15:09:50 -0700

    class & DOM attach

--- js/main.js
+++ js/main.js
@@ -1,2 +1,12 @@
 // do stuff
 console.log('yo');
+
+class Calculator {
+  constructor(element) {
+    this.numKeys = [];
+    this.opKeys = [];
+  }
+}
+
+const calcElement = document.querySelector('#calculator');
+const calc = new Calculator(calcElement);
