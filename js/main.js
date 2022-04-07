c802fdf55b934bae932fe848a9d7c963adb29759
Jim Nelson <jim@jimnelson.us>
2022-04-07 15:57:06 -0700

    got it, needed function.bind

--- js/main.js
+++ js/main.js
@@ -6,11 +6,11 @@ class Calculator {
     this.element = element;
     this.numKeys = [];
     this.opKeys = [];
+    this.display = document.createElement('section');
     this.initRender();
   }
 
   initRender() {
-    this.display = document.createElement('section');
     this.display.classList.add('display');
     this.display.innerHTML = '';
     this.keypad = document.createElement('section');
@@ -21,15 +21,15 @@ class Calculator {
     for (let i = 0; i < 10; i++) {
       const key = document.createElement('button');
       key.innerHTML = i;
-      key.addEventListener('click', this.numKeyPressed);
+      key.addEventListener('click', this.numKeyPressed.bind(this));
       this.keypad.appendChild(key);
     }
   }
-  numKeyPressed(event) {
-    console.log(event.target.innerHTML, this.display);
-    this.display.innerHTML += event.target.innerHTML;
-  }
 }
+Calculator.prototype.numKeyPressed = function (event) {
+  console.log(event.target.innerHTML, this.display);
+  this.display.innerHTML += event.target.innerHTML;
+};
 
 const calcElement = document.querySelector('#calculator');
 const calc = new Calculator(calcElement);
