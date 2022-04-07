0c35238d4b4a75768e81f11e1ce5b72e110ffe71
Jim Nelson <jim@jimnelson.us>
2022-04-07 15:20:41 -0700

    render dom, debug styling

--- js/main.js
+++ js/main.js
@@ -3,8 +3,19 @@ console.log('yo');
 
 class Calculator {
   constructor(element) {
+    this.element = element;
     this.numKeys = [];
     this.opKeys = [];
+    this.initRender();
+  }
+
+  initRender() {
+    this.display = document.createElement('section');
+    this.display.classList.add('display');
+    this.keypad = document.createElement('section');
+    this.keypad.classList.add('keypad');
+    this.element.appendChild(this.display);
+    this.element.appendChild(this.keypad);
   }
 }
 
